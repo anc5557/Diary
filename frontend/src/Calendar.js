97cd8e405c4fade75ef91ce5af4a523d9a535a4f
junho <anc5557@naver.com>
2023-11-09 16:59:15 +0900

    Add diary fetching and display functionality to Calendar component

--- frontend/src/Calendar.js
+++ frontend/src/Calendar.js
@@ -1,75 +1,93 @@
 // path : frontend/src/Calendar.js
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import moment from "moment";
 import ReactCalendar from 'react-calendar';
 import DiaryModal from "./DiaryModal";
+
 import 'react-calendar/dist/Calendar.css';
 import './custom-react-calendar.css';
 
 import "./App.css";
 
 function Calendar() {
-    const [diaries, setDiaries] = useState([]);
-    const [date, setDate] = useState(new Date());
-    const [showForm, setShowForm] = useState(false);
-    const [diaryContent, setDiaryContent] = useState("");
-    const [diaryImage, setDiaryImage] = useState(null);
+    const [diaries, setDiaries] = useState([]); // diaries 상태 추가(초기값은 빈 배열),     
+    const [date, setDate] = useState(new Date()); // date 상태 추가(초기값은 오늘 날짜)
+    const [showForm, setShowForm] = useState(false); // showForm 상태 추가(초기값은 false)
+    const [diaryData, setDiaryData] = useState(null); // 서버로부터 가져온 일기 데이터 상태
 
     const handleCloseModal = () => {
         setShowForm(false);
     };
 
-    const handleModalSubmit = async () => {
-        const formData = new FormData();
-        formData.append('date', date.toISOString().substring(0, 10));
-        formData.append('content', diaryContent);
-        if (diaryImage) {
-            formData.append('image', diaryImage);
-        }
-
-        try {
-            const res = await axios.post("http://localhost:3001/diaries", formData, {
-                headers: {
-                    'Content-Type': 'multipart/form-data',
-                },
-            });
-            setDiaries([...diaries, res.data]);
-            setShowForm(false);
-            setDiaryContent("");
-            setDiaryImage(null);
-        } catch (err) {
-            console.log(err);
-        }
-    };
-
+    // 캘린더 렌더링 시, 서버에서 일기, 날짜 정보를 가져옵니다.
     useEffect(() => {
-        const fetchDiaries = async () => {
-            try {
-                const res = await axios.get("http://localhost:3001/diaries");
-                setDiaries(res.data);
-            } catch (err) {
-                console.log(err);
-            }
-        };
-        fetchDiaries();
+        const token = localStorage.getItem('token');
+        if (!token) {
+            return;
+        }
+        axios.get('http://localhost:3001/diary/dates', {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+            .then((response) => {
+                // 서버로부터 받아온 날짜 데이터로 marks를 업데이트합니다.
+                const fetchedMarks = response.data.map(date => moment(date).format("DD-MM-YYYY"));
+                setDiaries(fetchedMarks);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
     }, []);
 
-    const handleDateClick = (value) => {
-        setDate(value);
-        setShowForm(true);
+    const handleDateClick = async (value) => {
+        // 클릭한 날짜를 포맷에 맞게 변환
+        const formattedDate = moment(value).format("YYYY-MM-DD");
+        
+        try {
+            // 서버에 해당 날짜의 일기 데이터를 요청
+            const response = await axios.get(`http://localhost:3001/diary/${formattedDate}`, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                },
+            });
+            
+            // 가져온 데이터를 diaryData 상태에 저장
+            setDiaryData(response.data);
+            setShowForm(true); // 모달을 표시
+        } catch (error) {
+            console.error('Error fetching diary entry:', error);
+            setDiaryData(null); // 에러가 발생했을 때 데이터 초기화
+            setShowForm(false); // 모달을 표시하지 않음
+        }
     };
+
+
+
 
     return (
         <div className="calendar-container">
             <h1>다이어리</h1>
-            <ReactCalendar onChange={handleDateClick} value={date} />
+            <ReactCalendar 
+                onChange={handleDateClick} value={date}  // onChange와 value props 추가 
+                // calendarType prop 추가
+                calendarType="gregory"
+                showNeighboringMonth={false}
+                tileContent={({ date, view }) => {
+                    // diaries 상태를 참조하여 날짜에 점을 찍습니다.
+                    if (diaries.find((x) => x === moment(date).format("DD-MM-YYYY"))) {
+                        return <div className="dot"></div>;
+                    }
+                }}  
+                
+                formatDay={(locale, date) => moment(date).format("DD")}
+
+            />
             <DiaryModal
                 show={showForm}
                 onClose={handleCloseModal}
-                onSubmit={handleModalSubmit}
-                onImageChange={(image) => setDiaryImage(image)}
-                onContentChange={(content) => setDiaryContent(content)}
-                content={diaryContent}
+                diaryData={diaryData} // 서버로부터 가져온 일기 데이터를 모달에 전달
             />
         </div>
     );
