5060492609c616e4e2d9e13396d5e199c194170f
junho <anc5557@naver.com>
2023-11-09 16:57:48 +0900

    Add Footer component to frontend

---
+++ frontend/src/Footer.js
@@ -0,0 +1,18 @@
+// path: frontend/src/Footer.js
+
+import React from 'react';
+import './App.css';
+
+function Footer() {
+    // 그냥 네모 박스로 만들어놓은 것
+    return (
+        <div className="footer">
+            <div className="footer-text">
+                <p>© 2023. All rights reserved.</p>
+            </div>
+        </div>
+    );
+    
+}
+
+export default Footer;
