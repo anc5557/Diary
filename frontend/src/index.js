48cfc2b10b023529a046dd35341221fb43960966
junho <anc5557@naver.com>
2023-11-09 18:50:42 +0900

    Refactor ReactDOM.render to use createRoot in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
-// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client'; // createRoot를 임포트합니다.
 import { Provider } from 'react-redux';
 import { store } from './store';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -9,13 +8,15 @@ import App from './App';
 
 let persistor = persistStore(store);
 
-ReactDOM.render(
+const container = document.getElementById('root'); // 루트 DOM 요소를 가져옵니다.
+const root = createRoot(container); // createRoot로 루트를 생성합니다.
+
+root.render( // render 메소드로 앱 컴포넌트를 렌더링합니다.
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
